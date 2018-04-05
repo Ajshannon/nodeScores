f91ec703dda9ba9a99855fbf57a8ac3bed096099
AJ Shannon <anjoshannon@gmail.com>
2018-04-05 15:08:39 -0400

    m

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const http = require('http');
-const hostname = '';
+const express = require('express');
+const app = express();
+const jsonBody = require('body/json');
 const port = 3000;
-const server = http.createServer((req, res) => {
+
+var body;
+var scores = [{
+  name: "Edwin",
+  score: 50
+}, {
+  name: "David",
+  score: 39
+}];
+
+app.get(!'/scores', (req, res) => res.statusCode = 404);
+  
+app.get('/scores', (req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Wassup Brev\n');
-  res.end()
-  console.log(req.headers);
-  console.log(req.method);
+  scores.sort((a, b) => (b.score - a.score));
+  scores = scores.splice(0, 3);
+  res.send(scores);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.post('/scores',(req, res) => {
+  res.statusCode = 201;
+  jsonBody(req, res, (err, body) => {
+      scores.push(body);
+  })
 });
 
+app.listen(port, () => console.log('Example app listening on port 3000!'))
